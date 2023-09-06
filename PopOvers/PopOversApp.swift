aabc1ab411df45e7e749c885a8da894be651a11e
CeciliaChen <cecilia@pdotf.com>
2023-09-06 16:40:01 -0400

    Initial Commit

---
+++ PopOvers/PopOversApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopOversApp.swift
+//  PopOvers
+//
+//  Created by Cecilia Chen on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PopOversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
